852de7354a8dc0b044fb01a3d2ce4f0173510a29
KhushiMa27 <khushimakhija27@gmail.com>
2024-10-08 21:24:07 +0530

    url changes

--- client/src/store/useUserStore.ts
+++ client/src/store/useUserStore.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 import { toast } from "sonner";
 import { LoginInputState, SignupInputState } from "@/schema/UserSchema";
 
-const API_END_POINT = "https://full-stack-restaurant-app.onrender/api/v1/user"
+const API_END_POINT = "https://full-stack-restaurant-app.onrender.com/api/v1/user"
 axios.defaults.withCredentials = true;
 
 type User = {
